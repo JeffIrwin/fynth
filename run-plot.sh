0dc66147c77b5f5216d71154157f49abbdd4da83
Jeff Irwin <jirwin505@gmail.com>
2025-04-13 19:52:40 -0400

    make the whole basename a variable, not just the suffix

--- run-plot.sh
+++ run-plot.sh
@@ -6,26 +6,29 @@ set -exu
 # Use a low frequency and a short length to avoid making too much data (and to
 # help make the plot readable)
 
-suffix="1"
+#********
 
-#fpm run -- sin-"$suffix".wav --square 30.0 0.1
-fpm run -- sin-"$suffix".wav --sine 30.0 0.1
+# Basename for generated data files with various extensions
+base="data-1"
 
-fpm run -- sin-"$suffix".wav sin-"$suffix".csv
+#fpm run -- "$base".wav --square 30.0 0.1
+fpm run -- "$base".wav --sine 30.0 0.1
+
+fpm run -- "$base".wav "$base".csv
 
 # `pause` hangs the bash script here, but allows interactive zooming.  you can
 # alternatively use `gnuplot -p -e ...` to keep the plot window open after
 # gnuplot exits, but then you can't zoom!
 #
-# To zoom in gnuplot, right-click and then left-click to draw the zoom rectangle
+# To zoom in gnuplot, right-click to draw the zoom rectangle
 
-#gnuplot -e 'plot "sin-'"$suffix"'.csv" with linespoints ; pause -1'
-#gnuplot -p -e 'plot "sin-'"$suffix"'.csv" with linespoints'
+#gnuplot -e 'plot "'"$base"'.csv" with linespoints ; pause -1'
+#gnuplot -p -e 'plot "'"$base"'.csv" with linespoints'
 
 # Save a png without making any window
 gnuplot -e '
 	set terminal png size 800,800; 
-	set output "sin-'"$suffix"'.png";
-	plot "sin-'"$suffix"'.csv" with linespoints
+	set output "'"$base"'.png";
+	plot "'"$base"'.csv" with linespoints
 '
 
